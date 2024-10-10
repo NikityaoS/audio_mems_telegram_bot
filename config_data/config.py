da50fab6605bc1ebf608e8a63d92b8f0396ac187
Nikitos <artemshpayzman@mail.ru>
2024-10-10 17:55:11 +0300

    в модуле config добавлены датаклассы MongoDB, Config_db, добавлена функция load_config_db

--- config_data/config.py
+++ config_data/config.py
@@ -1,14 +1,29 @@
 from dataclasses import dataclass
 from environs import Env
 
+
 @dataclass
 class TgBot:
     token: str
     admin_ids: list[int]
 
+
+@dataclass
+class MongDB:
+    user_name: str
+    password: str
+    db: str
+    auth_mech: str
+
+
 @dataclass
 class Config:
     tg_bot: TgBot
+
+
+@dataclass
+class Config_db:
+    mongodb: MongDB
 
 
 def load_config() -> Config:
@@ -17,4 +32,17 @@ def load_config() -> Config:
     return Config(
         tg_bot=TgBot(
             token=env('BOT_TOKEN'),
-            admin_ids=list(map(int, env.list('ADMIN_IDS')))))
+            admin_ids=list(map(int, env.list('ADMIN_ID')))))
+
+
+def load_config_db() -> Config_db:
+    env: Env = Env()
+    env.read_env()
+    return Config_db(
+        mongodb=MongDB(
+            user_name=env('MONGO_USERNAME'),
+            password=env('MONGO_PASSWORD'),
+            db=env('MONGO_DB'),
+            auth_mech=env('MONGO_AUTH_MECH')
+        )
+    )
